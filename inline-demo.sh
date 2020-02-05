921b873ece451c3553cc072b6297a5f752c6838c
https://github.com/cassigewer/SineWaveSpeech.git <cassigewer@gmail.com>
2020-02-05 11:24:13 +0200

    "adapt to mac"

--- inline-demo.sh
+++ inline-demo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v arecord)" ]; then
-  echo 'Error: arecord is not installed. Maybe you not using linux, adam.' >&2
-  exit 1
-fi
+#if ! [ -x "$(command -v arecord)" ]; then
+#  echo 'Error: arecord is not installed. Maybe you not using linux, adam.' >&2
+#  exit 1
+#fi
 
-if ! [ -x "$(command -v aplay)" ]; then
-  echo 'Error: aplay is not installed. Maybe you not using linux, adam.' >&2
-  exit 1
-fi
+#if ! [ -x "$(command -v aplay)" ]; then
+#  echo 'Error: aplay is not installed. Maybe you not using linux, adam.' >&2
+#  exit 1
+#fi
 
-if ! [ -x "$(command -v praat)" ]; then
-  echo 'Error: praat is not installed.' >&2
-  exit 1
-fi
+#if ! [ -x "$(command -v praat)" ]; then
+#  echo 'Error: praat is not installed.' >&2
+#  exit 1
+#fi
 
 echo -e "1) Get ready to record a 5 second wav"
 echo -e "   3..."
@@ -24,10 +24,12 @@ echo -e "   1..."
 sleep 1
 echo -e "   GO!"
 
-arecord -d5 input/sound.wav
+#arecord -d5 input/sound.wav --linux
+sox -d input/sound.wav
 
 echo -e "   cool!\n"
 
 ./run.sh
 
-aplay output/sound.wav
+#aplay output/sound.wav -- linux
+afplay output/sound.wav
--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v praat)" ]; then
-  echo 'Error: praat is not installed.' >&2
-  exit 1
-fi
+#if ! [ -x "$(command -v praat)" ]; then
+#  echo 'Error: praat is not installed.' >&2
+#  exit 1
+#fi
 
 if ! [ -f "input/sound.wav" ]; then
   echo 'Error: Source wav not present.' >&2
   exit 1
 fi
 
-praat --run src/praat.script Male 1 1 1 1 0 50 1 20 1
+/Applications/Praat.app/Contents/MacOS/Praat --run src/praat.script Female 1 1 1 1 0 50 1 20 1
