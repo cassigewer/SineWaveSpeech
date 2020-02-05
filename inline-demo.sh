6b94148852de3eb35ecd8df393606104065aebc4
StevenRonnyFrohlich <steve.r.frohlich@gmail.com>
2020-02-05 10:53:46 +0200

    got a functional SWS generator

---
+++ inline-demo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v arecord)" ]; then
+  echo 'Error: arecord is not installed. Maybe you not using linux, adam.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v aplay)" ]; then
+  echo 'Error: aplay is not installed. Maybe you not using linux, adam.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v praat)" ]; then
+  echo 'Error: praat is not installed.' >&2
+  exit 1
+fi
+
+echo -e "1) Get ready to record a 5 second wav"
+echo -e "   3..."
+sleep 1
+echo -e "   2..."
+sleep 1
+echo -e "   1..."
+sleep 1
+echo -e "   GO!"
+
+arecord -d5 input/sound.wav
+
+echo -e "   cool!\n"
+
+./run.sh
+
+aplay output/sound.wav
